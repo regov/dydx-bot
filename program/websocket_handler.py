c8ba9a7c339c4ec56fa87b540d668652ac247032
Raouf-Guechi <regov@iMac-de-Raouf.local>
2023-04-17 19:28:16 +0200

    fix websocket server IP

--- program/websocket_handler.py
+++ program/websocket_handler.py
@@ -12,7 +12,7 @@ async def websocket_handler(websocket, path):
         connected_clients.remove(websocket)
 
 async def start_websocket_server():
-    async with websockets.serve(websocket_handler, "localhost", 8765):
+    async with websockets.serve(websocket_handler, "0.0.0.0", 8765):
         print("WebSocket server started on ws://localhost:8765")
         await asyncio.Future()  # Exécutez le serveur WebSocket indéfiniment
 
